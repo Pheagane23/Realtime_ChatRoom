9f9fcdb95befe28197755b3044a0d5003a7f7d4a
Pheagane23 <Pheaganer2@gmail.com>
2023-08-29 14:45:57 +0200

    Realtime chat room on MVC using SignalR
    ....

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Chat_Function.Hubs;
+
+public class ChatHub : Hub
+{
+    public async Task SendMessage (string user,string message) 
+    {
+        Clients.All.SendAsync("RecieveMessage", user, message);
+    }
+}
